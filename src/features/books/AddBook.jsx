83ca9d4d0446d21fd05fa3c2b33382deda726884
MRJAHID <Mrjahid350@gmail.com>
2022-06-14 21:27:08 +0600

    Add Data

--- src/features/books/AddBook.jsx
+++ src/features/books/AddBook.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addBooks } from './BooksSlice';
 
 export default function AddBook() {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const noOfBooks = useSelector((state) => state.booksReducer.books.length);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const book = { id: noOfBooks + 1, title, author };
+        dispatch(addBooks(book));
+        navigate('/show-books', { replace: true });
+    };
     return (
         <div>
             <h2>AddBook</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-field">
+                    <label htmlFor="title">Title </label>
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        required
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+
+                <div className="form-field">
+                    <label htmlFor="author">Author </label>
+                    <input
+                        type="text"
+                        id="author"
+                        name="author"
+                        required
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Add Book</button>
+            </form>
         </div>
     );
 }
--- src/features/books/BooksView.jsx
+++ src/features/books/BooksView.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function BooksView() {
-    const books = useSelector(state => state.booksReducer.books)
-    console.log(books)
-    
-  return (
-    <div>
-        <h2>List Of Books</h2>
-    </div>
-  )
+    const books = useSelector((state) => state.booksReducer.books);
+    console.log(books);
+
+    return (
+        <div>
+            <h2>List Of Books</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books &&
+                        books.map((book) => {
+                            const { id, title, author } = book;
+                            return (
+                                <tr key={id}>
+                                    <td>{id}</td>
+                                    <td>{title}</td>
+                                    <td>{author}</td>
+                                    <td>
+                                        <button>Edit</button>
+                                        <button>Delete</button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                </tbody>
+            </table>
+        </div>
+    );
 }
